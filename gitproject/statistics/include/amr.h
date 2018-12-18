870ea217ee7d2903587cae66c8c8e1d7c3b24ae6
yuzejun <532570579@qq.com>
2018-12-18 17:36:44 +0800

    统计音频信息

--- gitproject/statistics/include/amr.h
+++
@@ -1,7 +0,0 @@
-typedef struct{
-    char frame_head;
-    char buf[];
-}frame;
-
-char   file_head[19];                               //存放文件头
-char    channel_number;                             //声道数
